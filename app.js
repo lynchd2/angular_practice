d3343f4e7f076e5be4e01d69ba8b412953cd3355
Dylan Lynch <lynchd2@tcnj.edu>
2016-09-25 23:12:05 -0400

    first commit

--- app.js
+++ app.js
@@ -4,28 +4,59 @@ app.controller("FirstCtrl", ["$scope", function($scope) {
 
   $scope.name = "hello!";
   $scope.meats = [
+
+  {
+    meat: "chicken",
+    rank: "4th",
+    publicRate: 95,
+    available: true,
+    thumb: "content/chicken.jpg"
+  },
+
   {
     meat: "ham",
     rank: "1st",
-    publicRate: 50
+    publicRate: 50,
+    available: true,
+    thumb: "content/ham.jpg"
   },
 
   {
     meat: "pork",
     rank: "2nd",
-    publicRate: 75
+    publicRate: 75,
+    available: true,
+    thumb: "content/pork.jpg"
   },
 
   {
     meat: "beef",
     rank: "3rd",
-    publicRate: 85
-  },
+    publicRate: 85,
+    available: false,
+    thumb: "content/beef.jpg"
+  }
+]
 
+  $scope.removeMeat = function(meat) {
+    for(var i = 0; i < $scope.meats.length; i++) {
+      if($scope.meats[i].meat === meat.meat) {
+        $scope.meats.splice(i,1);
+      }
+    }
+  }
 
-
-
-  ]
+  $scope.addMeat = function() {
+    $scope.meats.push( {
+      meat: $scope.newmeat.meat,
+      rank: $scope.newmeat.rank,
+      publicRate: parseInt($scope.newmeat.publicRate),
+      available: true
+    });
+    $scope.newmeat.meat = ""
+    $scope.newmeat.rank = ""
+    $scope.newmeat.publicRate = ""
+  }
   
 
 }])
